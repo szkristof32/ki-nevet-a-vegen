e67ee0a2990504cde167d1264d258bbe82b2b735
szkristof32 <kristofreg@gmail.com>
2025-10-24 15:36:38 +0200

    Game board part 2

--- ki-nevet-a-vegen/src/board.c
+++ ki-nevet-a-vegen/src/board.c
@@ -13,16 +13,27 @@ bool board_create(scene_t* scene, board_t* out_board)
 	out_board->game_object = scene_new_object(scene);
 	obj_loader_load_model("assets/models/board_placeholder.obj", &out_board->game_object->model.mesh);
 
+	const float board_size = 15.0f;
+	const float home_size = 1.75f;
+
 	for (uint32_t i = 0; i < 4; i++)
 	{
-		vec4 colour = vec4_create(i % 3 == 0, i % 2 == 1, i == 2, 1.0f);
+		float x_axis = (float)(-(i % 3 == 0) + (i % 3 != 0));
+		float y_axis = (float)(-(i % 2 == 0) + (i % 2 != 0));
+
+		vec3 base_position = vec3_create(x_axis * board_size, 0.0f, y_axis * board_size);
+		vec4 colour = vec4_create((float)(i % 3 == 0), (float)(i % 2 == 1), (float)(i == 2), 1.0f);
+
 		for (uint32_t j = 0; j < 4; j++)
 		{
+			float x_axis = (float)(-(j % 3 == 0) + (j % 3 != 0));
+			float y_axis = (float)(-(j % 2 == 0) + (j % 2 != 0));
+
 			game_object_t* object = out_board->piece_objects[i * 4 + j] = scene_new_object(scene);
 			obj_loader_load_model("assets/models/piece_placeholder.obj", &object->model.mesh);
 
 			object->colour = colour;
-			object->transform.position = vec3_create((i * 4 + j) * 2.5f, 0.0f, 0.0f);
+			object->transform.position = vec3_add(base_position, vec3_create(x_axis * home_size, 0.0f, y_axis * home_size));
 			object->transform.scale = vec3_scalar(0.125f);
 		}
 	}
