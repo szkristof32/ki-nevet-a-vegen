7353a0a55387c9793bd47ef7ee364f345a27a916
szkristof32 <kristofreg@gmail.com>
2025-11-11 21:26:03 +0100

    Documentation
    Part 2

--- infoc-engine/src/infoc/math/utilities.h
+++ infoc-engine/src/infoc/math/utilities.h
@@ -5,21 +5,126 @@
 
 #define PI (3.141592654f)
 
+/*
+* Converts degrees to radians
+*
+* @param deg Angle in degrees
+*
+* @returns Angle in radians
+*/
 float deg_to_rad(float deg);
+/*
+* Converts radians to degrees
+*
+* @param deg Angle in radians
+*
+* @returns Angle in degrees
+*/
 float rad_to_deg(float rad);
 
+/*
+* Creates a 4x4 translation matrix
+*
+* @param matrix Source matrix
+* @param vector Translation
+*
+* @returns The translation matrix
+*/
 mat4 mat4_translate(mat4 matrix, vec4 vector);
 
+/*
+* Creates a 4x4 translation matrix
+*
+* @param matrix Source matrix
+* @param x Translation along the x axis
+*
+* @returns The translation matrix
+*/
 mat4 mat4_translate_x(mat4 matrix, float x);
+/*
+* Creates a 4x4 translation matrix
+*
+* @param matrix Source matrix
+* @param y Translation along the y axis
+*
+* @returns The translation matrix
+*/
 mat4 mat4_translate_y(mat4 matrix, float y);
+/*
+* Creates a 4x4 translation matrix
+*
+* @param matrix Source matrix
+* @param z Translation along the z axis
+*
+* @returns The translation matrix
+*/
 mat4 mat4_translate_z(mat4 matrix, float z);
 
+/*
+* Creates a 4x4 rotation matrix
+*
+* @param matrix Source matrix
+* @param x Rotation in radians along the x axis
+*
+* @returns The rotation matrix
+*/
 mat4 mat4_rotate_x(mat4 matrix, float angle_rad);
+/*
+* Creates a 4x4 rotation matrix
+*
+* @param matrix Source matrix
+* @param y Rotation in radians along the y axis
+*
+* @returns The rotation matrix
+*/
 mat4 mat4_rotate_y(mat4 matrix, float angle_rad);
+/*
+* Creates a 4x4 rotation matrix
+*
+* @param matrix Source matrix
+* @param z Rotation in radians along the z axis
+*
+* @returns The rotation matrix
+*/
 mat4 mat4_rotate_z(mat4 matrix, float angle_rad);
 
+/*
+* Creates a 4x4 scale matrix
+*
+* @param matrix Source matrix
+* @param scalar Scalar
+*
+* @returns The scale matrix
+*/
 mat4 mat4_scale_vec3(mat4 matrix, vec3 scalar);
+/*
+* Creates a 4x4 scale matrix
+*
+* @param matrix Source matrix
+* @param scalar Scalar
+*
+* @returns The scale matrix
+*/
 mat4 mat4_scale_vec4(mat4 matrix, vec4 scalar);
 
+/*
+* Creates a 4x4 perspective projection matrix
+*
+* @param fov Field of view in radians
+* @param aspect_ratio The aspect ratio of the image
+* @param near_plane The distance of the near clipping plane
+* @param far_plane The distance of the far clipping plane
+*
+* @returns The projection matrix
+*/
 mat4 mat4_perspective(float fov, float aspect_ratio, float near_plane, float far_plane);
+/*
+* Creates a 4x4 view matrix where the camera always faces a center point
+*
+* @param eye The position of the camera
+* @param center The center point to be faced
+* @param up The unit vector along the up axis
+*
+* @returns The view matrix
+*/
 mat4 mat4_look_at(vec3 eye, vec3 center, vec3 up);
--- infoc-engine/src/infoc/math/vec3.h
+++ infoc-engine/src/infoc/math/vec3.h
@@ -2,6 +2,9 @@
 
 #include "infoc/core/defines.h"
 
+/*
+* Three component vector
+*/
 typedef struct vec3
 {
 	union
@@ -24,24 +27,133 @@ typedef struct vec3
 	};
 } vec3;
 
+/*
+* Creates a three component vector
+*
+* @param x X coordinate of the vector
+* @param y Y coordinate of the vector
+* @param z Z coordinate of the vector
+*
+* @returns The vector (x;y;z)
+*/
 vec3 vec3_create(float x, float y, float z);
+/*
+* Creates a three component vector
+*
+* @param scalar The x, y and z coordinates of the vector
+*
+* @returns The vector (scalar;scalar;scalar)
+*/
 vec3 vec3_scalar(float scalar);
+/*
+* Creates a three component vector
+*
+* @param other Source vector
+*
+* @returns The vector (other.x;other.y;other.z)
+*/
 vec3 vec3_vec3(vec3 other);
 
+/*
+* Adds together two vectors
+*
+* @param a, b Addition operands
+*
+* @returns The sum of a and b
+*/
 vec3 vec3_add(vec3 a, vec3 b);
+/*
+* Subtracts a vector from the other
+*
+* @param a Minuend vector
+* @param b Subtrahend vector
+*
+* @returns The difference of a and b
+*/
 vec3 vec3_sub(vec3 a, vec3 b);
 
+/*
+* Scales a vector by a scalar value
+*
+* @param a Source vector
+* @param scalar The scalar to multiply with
+*
+* @returns The scaled vector
+*/
 vec3 vec3_scale(vec3 a, float scalar);
+/*
+* Multiplies two vectors per component resulting in (a.x*b.x;a.y*b.y;a.z*b.z)
+*
+* @param a, b The operands of the multiplication
+*
+* @returns The product
+*/
 vec3 vec3_mul(vec3 a, vec3 b);
+/*
+* Raises each component of the vector to the second power
+*
+* @param vector Vector subject to squaring
+*
+* @returns The resulting vector
+*/
 vec3 vec3_square(vec3 vector);
+/*
+* Inverts each component of the vector
+*
+* @param vector Vector to be inverted
+*
+* @returns The inverted vector
+*/
 vec3 vec3_inv(vec3 vector);
 
+/*
+* Calculates the dot product of two vectors
+*
+* @param a, b Two vectors
+*
+* @returns The dot product of a and b
+*/
 float vec3_dot(vec3 a, vec3 b);
+/*
+* Calculates the cross product of two vectors
+*
+* @param a, b Two vectors
+*
+* @returns The cross product of a and b
+*/
 vec3 vec3_cross(vec3 a, vec3 b);
 
+/*
+* Calculates the length of the vector raised to the second power
+*
+* @param vector The vector whose length is to be calculated
+*
+* @returns The length squared
+*/
 float vec3_length_squared(vec3 vector);
+/*
+* Calculates the length of the vector
+*
+* @param vector The vector whose length is to be calculated
+*
+* @returns The length
+*/
 float vec3_length(vec3 vector);
 
+/*
+* Normalises the vector
+*
+* @param vector The vector which needs to be normalised
+*
+* @returns The normalised vector
+*/
 vec3 vec3_normalise(vec3 vector);
 
+/*
+* Determines if two vectors are equal
+*
+* @param a, b The vectors to be checked
+*
+* @returns A boolean indicating if they are equal
+*/
 bool vec3_eq(vec3 a, vec3 b);
